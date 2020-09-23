5a5d044f98a5b1598353511a98b92f9c9b92881b
Oliver Sandli <56598098+oliversandli@users.noreply.github.com>
2020-09-23 18:31:15 -0500

    Improve help, version, and usage messages.

--- nff.nim
+++ nff.nim
@@ -3,6 +3,10 @@ import parseopt
 import strutils
 import system
 
+# GLOBAL VARIABLES
+let version = "0.0.1"
+
+# PROCEDURES
 proc confirm_extension(conf_dir: string, file_name: string): string =
   # get file_name's extension
   var ext = file_name.split('.')[^1] # MARK let change
@@ -17,9 +21,25 @@ proc copy_paste_conf(source_name: string, dest_name: string) =
   var source_contents = readFile(source_name) # MARK let change
   writeFile(dest_name, source_contents)
 
+proc echo_version() =
+  echo "NimFormattedFile v" & version
+
+proc echo_usage() =
+  echo "usage: nff [options] output_file"
+
+proc echo_help() =
+  echo_version()
+  echo "Create a new file designated by the input file extension."
+  echo_usage()
+  echo """options:
+    -v --version  output current version
+    -h --help     ask for help"""
+
+# MAIN PROGRAM
 var parser = initOptParser()
 if paramCount() != 1:
   echo "Error: nff takes 1 argument, but ", paramCount(), " were given."
+  echo_usage()
   quit(1)
 
 # define flags and options
@@ -47,9 +67,11 @@ for kind, key, val in parser.getopt():
         program_state.version_flag = true
       else:
         echo "Error: invalid option '", key, "'"
+        echo_usage()
         break
     else:
       echo "Error: invalid option '", key, "', = '", val, "'"
+      echo_usage()
       break
   # arguments
   of cmdArgument:
@@ -57,13 +79,9 @@ for kind, key, val in parser.getopt():
 
 # output results
 if program_state.help_flag:
-  echo """NimFormattedFile v0.0.0
-Create a new file designated by the input file extension.
-options:
-    -v --version  output current version
-    -h --help     ask for help"""
+  echo_help()
 elif program_state.version_flag:
-  echo "NimFormattedFile v0.0.1"
+  echo_version()
 elif program_state.output_file != "":
   var conf_dir = case existsEnv("XDG_DATA_HOME") # MARK let change
     of true: getEnv("XDG_DATA_HOME") & "/nff"
