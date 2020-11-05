558ebc0abff9e648aa711f6ca27581dd3479a86e
Oliver Sandli <56598098+oliversandli@users.noreply.github.com>
2020-11-05 12:07:27 -0600

    Change MARK to TODO

--- nff.nim
+++ nff.nim
@@ -11,16 +11,16 @@ let version = "0.0.3"
 # PROCEDURES
 proc confirm_extension(conf_dir: string, file_name: string): string =
   # get file_name's extension
-  var ext = file_name.split('.')[^1] # MARK let change
+  var ext = file_name.split('.')[^1] # TODO: let change
   # generate the file path of the extention's config
-  var conf_path = conf_dir & "/templates/" & ext & ".tmpl" # MARK let change
+  var conf_path = conf_dir & "/templates/" & ext & ".tmpl" # TODO: let change
   if not fileExists(conf_path):
     echo "Could not find file '", conf_path
   else:
     return conf_path
 
 proc copy_paste_conf(source_name: string, dest_name: string) =
-  var source_contents = readFile(source_name) # MARK let change
+  var source_contents = readFile(source_name) # TODO: let change
   writeFile(dest_name, source_contents)
 
 # MAIN PROGRAM
@@ -33,7 +33,7 @@ parser.addRequiredArgument("file", "The file to create.")
 let args = parser.parse()
 let output_file = args["file"]
 
-var conf_dir = case existsEnv("XDG_DATA_HOME") # MARK let change
+var conf_dir = case existsEnv("XDG_DATA_HOME") # TODO: let change
   of true: getEnv("XDG_DATA_HOME") & "/nff"
   else: getEnv("HOME") & "/.config/nff"
 
