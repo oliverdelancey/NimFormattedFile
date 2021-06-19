5f21f24e16b31ed86f8f47c28995dabe8bb1e46b
Oliver C. Sandli <oliversandli@icloud.com>
2021-06-19 11:55:11 -0500

    Change var to let where possible

--- nff.nim
+++ nff.nim
@@ -12,16 +12,16 @@ let version = "0.0.3"
 # PROCEDURES
 proc confirm_extension(conf_dir: string, file_name: string): string =
   # get file_name's extension
-  var ext = file_name.split('.')[^1] # TODO: let change
+  let ext = file_name.split('.')[^1]
   # generate the file path of the extention's config
-  var conf_path = conf_dir & "/templates/" & ext & ".tmpl" # TODO: let change
+  let conf_path = conf_dir & "/templates/" & ext & ".tmpl"
   if not fileExists(conf_path):
     echo "Could not find file '", conf_path, "'"
   else:
     return conf_path
 
 proc copy_paste_template(source_name: string, dest_name: string) =
-  var source_contents = readFile(source_name) # TODO: let change
+  let source_contents = readFile(source_name)
   writeFile(dest_name, source_contents)
 
 # MAIN PROGRAM
@@ -34,11 +34,11 @@ parser.addRequiredArgument("file", "The file to create.")
 let args = parser.parse()
 let output_file = args["file"]
 
-var conf_dir = case existsEnv("XDG_DATA_HOME") # TODO: let change
+let conf_dir = case existsEnv("XDG_DATA_HOME") # TODO: let change
   of true: getEnv("XDG_DATA_HOME") & "/nff"
   else: getEnv("HOME") & "/.config/nff"
 
-var src = confirm_extension(conf_dir, output_file)
+let src = confirm_extension(conf_dir, output_file)
 if src != "":
   copy_paste_template(src, output_file)
 
